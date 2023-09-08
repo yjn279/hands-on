ea3a0be27201a8f5b9a32f8f15dc97edad4fbf41
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2023-09-09 00:23:35 +0900

    feat: create task service and resolver

--- udemy/nestjs-react-graphql/backend/src/task/task.module.ts
+++ udemy/nestjs-react-graphql/backend/src/task/task.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TaskResolver } from './task.resolver';
+import { TaskService } from './task.service';
 
 @Module({
-  providers: []
+  providers: [TaskResolver, TaskService]
 })
 export class TaskModule {}
---
+++ udemy/nestjs-react-graphql/backend/src/task/task.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { TaskService } from './task.service';
+import { Task } from './models/task.model';
+
+@Resolver()
+export class TaskResolver {
+    constructor(private readonly taskService: TaskService) {}
+
+    @Query(() => [Task], { nullable: 'items'})
+    getTasks(): Task[] {
+        return this.taskService.getTasks();
+    }
+}
