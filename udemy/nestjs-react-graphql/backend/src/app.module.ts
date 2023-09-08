772c4745d5d6df51678688bd454b583387699579
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2023-09-09 00:07:20 +0900

    chore: Setup GraphQL

--- udemy/nestjs-react-graphql/backend/src/app.module.ts
+++ udemy/nestjs-react-graphql/backend/src/app.module.ts
@@ -1,6 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      cors: {
+        origin: '*',
+      },
+    }),
+  ],
 })
 export class AppModule {}
