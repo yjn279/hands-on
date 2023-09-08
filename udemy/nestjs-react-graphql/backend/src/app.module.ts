0f10c3ed3203a84b82f660f1170dc5e6b98c81a1
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2023-09-09 00:17:14 +0900

    chore: create task model

--- udemy/nestjs-react-graphql/backend/src/app.module.ts
+++ udemy/nestjs-react-graphql/backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
@@ -9,10 +10,8 @@ import { join } from 'path';
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      cors: {
-        origin: '*',
-      },
     }),
+    TaskModule,
   ],
 })
 export class AppModule {}
