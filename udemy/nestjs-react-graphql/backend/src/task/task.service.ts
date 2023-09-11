353cd60653c946a5743e6e94944fc5c1b98c63f7
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2023-09-11 23:01:09 +0900

    Add DB to Task module

--- udemy/nestjs-react-graphql/backend/src/task/task.service.ts
+++ udemy/nestjs-react-graphql/backend/src/task/task.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { Task } from './models/task.model';
-import { CreateTaskInput } from './dto/createTask.input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskInput  } from './dto/createTask.input';
 
 @Injectable()
 export class TaskService {
-    tasks: Task[] = [];
+    constructor(private readonly prismaService: PrismaService) {}
 
-    getTasks(): Task[] {
-        return this.tasks;
+    async getTasks(): Promise<Task[]> {
+        return await this.prismaService.task.findMany();
     }
 
-    createTask(createTaskInput: CreateTaskInput): Task {
+    async createTask(createTaskInput: CreateTaskInput): Promise<Task> {
         const { name, dueDate, description } = createTaskInput;
-        
-        const newTask = new Task();
-        newTask.id = this.tasks.length + 1;
-        newTask.name = name;
-        newTask.dueDate = dueDate;
-        newTask.status = 'NOT_STARTED';
-        newTask.description = description;
-
-        this.tasks.push(newTask);
-        return newTask;
+        return await this.prismaService.task.create({
+            data: {
+                name,
+                dueDate,
+                description,
+            },
+        });
     }
 }
